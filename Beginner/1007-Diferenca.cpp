1358e7438fa6fb09a8e58c53c7abfa5e5638e152
caio guimaraes <caio.b.vilar@gmail.com>
2017-03-11 19:24:33 -0300

    Added Beginner folder and 7th test, first I'm documenting here.

---
+++ Beginner/1007-Diferenca.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int A,B,C,D;
+	std::cin >> A;
+	std::cin >> B;
+	std::cin >> C;
+	std::cin >> D;
+	std::cout << "DIFERENCA = " << (A*B) - (C*D) << std::endl;
+	return 0;
+}
---
+++ Beginner/build-1007.sh
@@ -0,0 +1,1 @@
+g++ 1007-Diferenca.cpp -o 1007-Diferenca -ggdb -Wall -Werror -std=c++11

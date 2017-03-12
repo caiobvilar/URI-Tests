8ed30bf0777ef8935d1f7d897ee1055caaf9d8a6
caio guimaraes <caio.b.vilar@gmail.com>
2017-03-12 12:15:29 -0300

    changing and putting Copyright notice on all sources xD

--- Beginner/1011/1011-Sphere.cpp
+++ Beginner/1011/1011-Sphere.cpp
@@ -1,3 +1,22 @@
+/*
+		Program Name: 1011-Sphere
+		Description: this program is a solution to the URI-Online Judge Beginners
+		Challenges, in particular the Beginner Challenge 1011.
+    Copyright (C) 2017  Caio José Borba Vilar Guimarães <caio.b.vilar@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -9,7 +28,7 @@ int main()
 	const double pi = 3.14159;
 	double volume;
 	std::cin >> radius;
-	volume = (4.0/3.0)*pi*pow(radius,3.0); 
+	volume = (4.0/3.0)*pi*pow(radius,3.0);
 //	std::fesetround(FE_DOWNWARD);
 	std::cout << "VOLUME = " << std::fixed << std::setprecision(3) << volume << std::endl;
 	return 0;
--- Beginner/1018/1018-Banknotes.cpp
+++ Beginner/1018/1018-Banknotes.cpp
@@ -1,3 +1,22 @@
+/*
+	 	Program Name: 1018-Banknotes
+		Description: this program is a solution to the URI-Online Judge Beginners
+		Challenges, in particular the Beginner Challenge 1018.
+		Copyright (C) 2017  Caio José Borba Vilar Guimarães <caio.b.vilar@gmail.com>
+
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <iostream>
 #include <iomanip>
 
@@ -8,70 +27,49 @@ int main()
 	nota_1 = nota_2 = nota_5 = nota_10 = nota_20 =  nota_50 = nota_100 = 0;
 	std::cin >> n;
 	aux = n;
-	if(n>=100)
+	while(n >=100)
 	{
-		while(n >=100)
-		{
-			nota_100+=1;
-			n = n-100;
-		}
+		nota_100+=1;
+		n = n-100;
 	}
-	if(n>=50)
+	while(n>=50)
 	{
-		while(n>=50)
-		{
-			nota_50+=1;
-			n = n-50;
-		}
+		nota_50+=1;
+		n = n-50;
 	}
-	if(n>=20)
+	while(n>=20)
 	{
-		while(n>=20)
-		{
-			nota_20 +=1;
-			n = n-20;
-		}
+		nota_20 +=1;
+		n = n-20;
 	}
-	if(n>=10)
+	while(n>=10)
 	{
-		while(n>=10)
-		{
-			nota_10 +=1;
-			n = n-10;
-		}
+		nota_10 +=1;
+		n = n-10;
 	}
-	if(n>=5)
+	while(n>=5)
 	{
-		while(n>=5)
-		{
-			nota_5 +=1;
-			n = n-5;
-		}
+		nota_5 +=1;
+		n = n-5;
 	}
-	if(n>=2)
+	while(n>=2)
 	{
-		while(n>=2)
-		{
-			nota_2 +=1;
-			n = n-2;
-		}
+		nota_2 +=1;
+		n = n-2;
 	}
-	if(n>=1)
+	while(n>=1)
 	{
-		while(n>=1)
-		{
-			nota_1+=1;
-			n = n-1;
-		}
+		nota_1+=1;
+		n = n-1;
 	}
 
 	std::cout << aux << std::endl
-						<< nota_100 << " nota(s) de R$ 100,00" << std::endl
-						<< nota_50 << " nota(s) de R$ 50,00" << std::endl
-						<< nota_20 << " nota(s) de R$ 20,00" << std::endl
-						<< nota_10 << " nota(s) de R$ 10,00" << std::endl
-						<< nota_5 << " nota(s) de R$ 5,00" << std::endl
-						<< nota_2 << " nota(s) de R$ 2,00" << std::endl
-						<< nota_1 << " nota(s) de R$ 1,00" << std::endl;
+		<< nota_100 << " nota(s) de R$ 100,00" << std::endl
+		<< nota_50 << " nota(s) de R$ 50,00" << std::endl
+		<< nota_20 << " nota(s) de R$ 20,00" << std::endl
+		<< nota_10 << " nota(s) de R$ 10,00" << std::endl
+		<< nota_5 << " nota(s) de R$ 5,00" << std::endl
+		<< nota_2 << " nota(s) de R$ 2,00" << std::endl
+		<< nota_1 << " nota(s) de R$ 1,00" << std::endl;
 	return 0;
 }
